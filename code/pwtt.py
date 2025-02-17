2a7e379faccc5d50734d8623b7b1673051e7303e
Ollie Ballinger <58981760+oballinger@users.noreply.github.com>
2025-02-17 10:17:02 +0100

    fixed

--- code/pwtt.py
+++ code/pwtt.py
@@ -101,10 +101,11 @@ def filter_s1(aoi,inference_start,war_start, pre_interval=12, post_interval=2, f
         .filter(ee.Filter.listContains("transmitterReceiverPolarisation", "VH")) \
         .filter(ee.Filter.eq("instrumentMode", "IW")) \
         .filterBounds(aoi) \
-        #.filter(ee.Filter.contains('.geo', aoi.geometry())) \
         .filterDate(ee.Date(inference_start), ee.Date(inference_start).advance(post_interval, 'months')) \
         .aggregate_array('relativeOrbitNumber_start') \
         .distinct()
+            #.filter(ee.Filter.contains('.geo', aoi.geometry())) \
+
     #orbits.getInfo()  # Print the orbits
 
     def map_orbit(orbit):
